941b2bad51896e55c13de20ff892f77126010564
Amir Bizimana <amirbzmn@gmail.com>
2019-02-10 20:43:33 -0500

    initial commit

---
+++ src/components/common/table.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import TableHeader from "./tableHeader.jsx";
+import TableBody from "./tableBody.jsx";
+
+const Table = ({ columns, sortColumn, onSort, data }) => {
+  return (
+    <table className="table">
+      <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
+      <TableBody columns={columns} data={data} />
+    </table>
+  );
+};
+
+export default Table;
